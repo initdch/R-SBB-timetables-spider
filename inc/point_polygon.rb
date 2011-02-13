31bbd1444adeea6c75f40844ecbd9c73d6992845
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2011-02-13 17:03:04 +0000

    Added missing library
    git-svn-id: svn://127.0.0.1/Users/vasile/svn-repo/personal/sbb-crawler-ruby@1308 27bc3c0c-e55f-d841-875c-cbd405bf67c2

---
+++ inc/point_polygon.rb
@@ -0,0 +1,38 @@
+require "csv"
+
+class Polygon
+  def initialize
+    @vertices = []
+  end
+
+  def load_from_file(file)
+    CSV.open(file, 'r', ',') do |row|
+      p = {"x" => row[0].to_f, "y" => row[1].to_f}
+      @vertices.push(p)
+    end
+  end
+
+  def contains_point?(point)
+    px = point['x']
+    py = point['y']
+    
+    inPolygon = false
+    i = 0
+    j = @vertices.length - 1
+    @vertices.each do |v|
+      v1x = v['x']
+      v1y = v['y']
+      v2x = @vertices[j]['x']
+      v2y = @vertices[j]['y']
+      
+      if (((v1y > py) != (v2y > py)) && (px < (v2x - v1x) * (py - v1y) / (v2y - v1y) + v1x))
+        inPolygon = ! inPolygon
+      end
+      
+      j = i
+      i = i + 1
+    end
+    
+    return inPolygon
+  end
+end
